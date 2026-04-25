d29ef1226918407eb6557acce55191b054b640d0
hodahassan2388-blip <hoda.hassan2388@gmail.com>
2026-04-25 14:51:49 +0300

    Update script.js

--- script.js
+++ script.js
@@ -7,15 +7,15 @@ function closeMenu() {
   document.getElementById("sidebar").style.left = "-220px";
   document.getElementById("overlay").style.display = "none";
 }
- const menuBtn = document.querySelector('.line'); // زرار الـ 3 شرط
-    const navMenu = document.querySelector('nav');   // القائمة
+ const menuBtn = document.querySelector('.line'); 
+    const navMenu = document.querySelector('nav');   
 
     menuBtn.addEventListener('click', (e) => {
-        e.stopPropagation(); // عشان الضغطة متتأثرش بحاجات تانية
-        navMenu.classList.toggle('active'); // يضيف أو يشيل كلاس active
+        e.stopPropagation(); 
+        navMenu.classList.toggle('active'); 
     });
 
-    // اختياري: لو دوست في أي مكان في الصفحة والقائمة مفتوحة.. تقفل
+   
     document.addEventListener('click', (e) => {
         if (!navMenu.contains(e.target) && navMenu.classList.contains('active')) {
             navMenu.classList.remove('active');
